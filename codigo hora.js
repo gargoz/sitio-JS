e157f89f7a96051a3702b4e27f796e6d8ffe579f
Cristhian Sánchez <81829087+gargoz@users.noreply.github.com>
2023-01-25 15:25:03 -0600

    first commit

---
+++ codigo hora.js
@@ -0,0 +1,67 @@
+/*let d = new Date();
+document.body.innerHTML = "<h1>Time right now is: " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()
+"</h1>"*/
+
+
+/*function pruebaemail (valor){
+	re=/^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+	if(!re.exec(valor)){
+		alert('email ERRÓNEO, inténtalo de nuevo');
+	}
+	else alert('email CORRECTO');
+	}
+
+---------------------------
+/*CREACIÓN DE VARIABLES:*/
+
+var producto="Refresco de 1 litro";
+
+var perro="raza bulldog";
+
+var gato="raza egipcia";
+
+var _perro="raza maltés";
+
+var perro_="raza x";
+
+perro_1="raza x";
+
+perro1="raza x";
+
+var Perro_chico;
+
+let perro_5="raza chihuaha";
+
+
+const perro_2="raza desconocida";
+
+	
+	//console.log(perro_5 +", "+ perro_2);
+
+	//----------------------- CREACIÓN DE ALERTAS
+
+	var alerta_1="BIENVENIDOS A MI SITIO";
+
+	var alerta_2='Para comenzar da clic en aceptar';
+
+	var alerta_3=String("Este es un constructor");
+
+	var alerta_4=new String("Este es un constructor");
+
+	var Monitor="Monitor 20\"";
+
+	var producto1=200;
+	var producto2=500;
+
+
+
+
+	console.log(producto1+producto2);
+
+//alert(Monitor);
+
+//alert(alerta_2);
+
+	//Mostrar fecha y hora:
+
+document.getElementById("fecha").innerHTML = Date();
